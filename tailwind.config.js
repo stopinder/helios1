161fb3189b0cf7aac02e5a7355865a349555513a
stopinder <chrysalisifs@robormiston.com>
2025-06-13 14:18:09 +0700

    Clean up Tailwind config to fix purge

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,8 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
     content: [
         "./**/*.html",
-        "./src/**/*.{vue,js,ts,jsx,tsx}",
+        "./src/**/*.{vue,js,ts,jsx,tsx}"
     ],
     theme: {
         extend: {},
