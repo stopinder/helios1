4a5f075d59d7d1b73c78e27c2843985a457cf4e9
stopinder <chrysalisifs@robormiston.com>
2025-06-19 10:36:58 +0700

    Fix Tailwind content paths

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,10 @@ const path = require('path');
 module.exports = {
     content: [
         path.join(__dirname, 'index.html'),
-        path.join(__dirname, 'src/**/*.{vue,js,ts,jsx,tsx}')
+        path.join(__dirname, 'src/**/*.{vue,js,ts,jsx,tsx,html}'),
+        path.join(__dirname, 'Views/**/*.{vue,js,ts,jsx,tsx,html}'),
+        path.join(__dirname, '*.html'),
+        path.join(__dirname, '*.js')
     ],
     theme: {
         extend: {},
